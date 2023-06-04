b25d91670e319ec9ae2ef85a9e5f03f8cedfbc4f
Tom Fan <fanhoman123@gmail.com>
2023-06-04 17:34:12 +0800

    update middleware

--- middleware.ts
+++ middleware.ts
@@ -2,12 +2,14 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
+  const res = NextResponse.next();
+
   const regx = new RegExp("/api/*");
 
   if (regx.test(request.url)) {
+    // Add Cross-Origin-Opener-Policy header to API response
+    res.headers.set("Cross-Origin-Opener-Policy", "same-origin");
   }
-
-  const origin = request.headers.get("orgin");
 
   return NextResponse.next();
 }
