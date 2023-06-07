b8edbc1a0f8de71900f3de7617a3d3af225c8612
Tom Fan <fanhoman123@gmail.com>
2023-06-08 00:45:15 +0800

    update

--- app/service/page.tsx
+++ app/service/page.tsx
@@ -1,7 +1,31 @@
-import Maintenance from "@/components/others/Maintenance";
+import { GridItem } from "@/components/chakra/LayoutComponents";
+import HeroBanner from "@/components/chakra/ui/HeroBanner";
+import SectionCard from "@/components/chakra/ui/SectionCard";
+import GridWrapper from "@/components/container/GridWrapper";
+import MotionContainer from "@/components/container/MotionContainer";
 import React from "react";
 
+const Spacer = () => {
+  return <GridItem py={{ base: "8", md: "16" }} colSpan={2}></GridItem>;
+};
+
 const Service: React.FC = () => {
-  return <Maintenance />;
+  return (
+    <>
+      <HeroBanner
+        image="/images/about.png"
+        mobileImage="/images/about_mobile.png"
+        title=""
+        message=""
+      />
+      <MotionContainer>
+        <GridWrapper>
+          <GridItem colSpan={2} mx="10">
+            <SectionCard />
+          </GridItem>
+        </GridWrapper>
+      </MotionContainer>
+    </>
+  );
 };
 export default Service;
