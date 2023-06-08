d4b6404927b6e127427cdec880260cd99260168d
Tom Fan <119341778+TomFanHM@users.noreply.github.com>
2023-06-08 07:46:48 +0000

    update

--- middleware.ts
+++ middleware.ts
@@ -33,15 +33,7 @@ export function middleware(request: NextRequest) {
   return res;
 }
 
+// Limit the middleware to paths starting with `/api/`
 export const config = {
-  matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - api (API routes)
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     */
-    "/((?!api|_next/static|_next/image|favicon.ico).*)",
-  ],
+  matcher: '/api/:function*',
 };
