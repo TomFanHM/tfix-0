610c355627a3ff8aca3e95d5097eaf4ab06e5153
Tom Fan <119341778+TomFanHM@users.noreply.github.com>
2023-06-28 06:52:04 +0000

    update

--- app/(top-down)/blogs/[slug]/page.tsx
+++ app/(top-down)/blogs/[slug]/page.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import { getComments, getPostById, getPosts } from "../_components/getPosts";
 import { notFound } from "next/navigation";
 import Maintenance from "@/components/others/Maintenance";
+import PostContainer from "../_components/post/PostContainer";
 
 export const revalidate = 0; //server side rendering
 
@@ -45,6 +46,8 @@ const PostDetail = async ({
 
   const { post, comments } = results;
 
-  return <Maintenance />;
+  return (
+    <PostContainer post={post} comments={comments} />
+  )
 };
 export default PostDetail;
