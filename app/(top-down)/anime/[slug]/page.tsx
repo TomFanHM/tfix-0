8717a4fcf0a6a4b95dbd0f88470e2cf47d901fc4
Tom Fan <fanhoman123@gmail.com>
2023-07-12 00:17:51 +0800

    update

--- app/(top-down)/anime/[slug]/page.tsx
+++ app/(top-down)/anime/[slug]/page.tsx
@@ -1,4 +1,4 @@
-import React, { cache } from "react";
+import React from "react";
 
 import { notFound } from "next/navigation";
 import MotionContainer from "@/components/container/MotionContainer";
@@ -9,26 +9,6 @@ import { getAnimes, getAnimeById } from "../_components/getAnimes";
 import { getProducts } from "../_components/getProducts";
 
 export const revalidate = 86400; //3600 * 24;
-
-export const getAnime = cache(async (id: string) => {
-  const anime = await getAnimeById(id);
-  return anime;
-});
-
-type Props = {
-  params: { slug: string };
-};
-
-export async function generateMetadata({ params }: Props) {
-  const anime = await getAnime(params.slug);
-
-  if (!anime) return {};
-
-  return {
-    title: anime.title_english || anime.title_japanese || "unknown",
-    description: anime.synopsis || "unknown",
-  };
-}
 
 export async function generateStaticParams() {
   const animesRef = collection(firestore, "animes");
@@ -45,7 +25,7 @@ const SelectedAnime = async ({
 }: {
   params: { slug: string };
 }): Promise<JSX.Element> => {
-  const anime = await getAnime(params.slug);
+  const anime = await getAnimeById(params.slug);
 
   if (!anime) notFound();
   const productsData = await getProducts(anime.title_english);
