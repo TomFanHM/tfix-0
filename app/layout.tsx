02c8d27df3aadbec1ad459edf002159f60d7438f
Tom Fan <fanhoman123@gmail.com>
2023-07-15 19:51:50 +0800

    update icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -59,7 +59,7 @@ export const metadata: Metadata = {
   },
   icons: {
     icon: "/favicon.ico",
-    shortcut: "/favicon-16x16.png",
+    shortcut: "/favicon.ico",
     apple: "/apple-touch-icon.png",
   },
   manifest: `${siteConfig.url}/site.webmanifest`,
